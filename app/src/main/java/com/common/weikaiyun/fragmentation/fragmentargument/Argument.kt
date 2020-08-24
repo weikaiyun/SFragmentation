aeed5936df5e69994d4d8bb225a08485180a4a8e
weikaiyun <weikaiyun@pwrd.com>
2020-08-24 10:10:38 +0800

    fragment传参工具

---
+++ app/src/main/java/com/common/weikaiyun/fragmentation/fragmentargument/Argument.kt
@@ -0,0 +1,9 @@
+package com.common.weikaiyun.fragmentation.fragmentargument
+
+import androidx.fragment.app.Fragment
+import kotlin.properties.ReadWriteProperty
+
+fun <T : Any> argument(): ReadWriteProperty<Fragment, T> =
+    FragmentArgumentDelegate()
+fun <T : Any> argumentNullable(): ReadWriteProperty<Fragment, T?> =
+    FragmentNullableArgumentDelegate()
---
+++ app/src/main/java/com/common/weikaiyun/fragmentation/fragmentargument/FragmentNullableArgumentDelegate.kt
@@ -0,0 +1,29 @@
+package com.common.weikaiyun.fragmentation.fragmentargument
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import kotlin.properties.ReadWriteProperty
+import kotlin.reflect.KProperty
+
+class FragmentNullableArgumentDelegate<T : Any?> :
+    ReadWriteProperty<Fragment, T?> {
+
+    @Suppress("UNCHECKED_CAST")
+    override fun getValue(
+        thisRef: Fragment,
+        property: KProperty<*>
+    ): T? {
+        val key = property.name
+        return thisRef.arguments?.get(key) as? T
+    }
+
+    override fun setValue(
+        thisRef: Fragment,
+        property: KProperty<*>, value: T?
+    ) {
+        val args = thisRef.arguments
+            ?: Bundle().also(thisRef::setArguments)
+        val key = property.name
+        value?.let { args.put(key, it) } ?: args.remove(key)
+    }
+}
