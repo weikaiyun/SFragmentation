79be13c4914154d895c2fac7e994fdffc8d88c18
weikaiyun <weikaiyun1990@outlook.com>
2020-09-05 19:02:14 +0800

    Demo

--- app/src/main/java/com/common/weikaiyun/demo/ui/DemoFragment4.kt
+++ app/src/main/java/com/common/weikaiyun/demo/ui/DemoFragment4.kt
@@ -5,16 +5,18 @@ import android.view.View
 import com.common.weikaiyun.R
 import com.common.weikaiyun.fragmentation.SupportFragment
 import com.common.weikaiyun.fragmentation.fragmentargument.argument
-import kotlinx.android.synthetic.main.fragment_demo.*
+import kotlinx.android.synthetic.main.fragment_demo4.*
 
 class DemoFragment4: SupportFragment() {
     private var param1: Int by argument()
     private var param2: String by argument()
+    private var param3: String by argument()
     companion object {
-        fun newInstance(param1: Int, param2: String): DemoFragment4 =
+        fun newInstance(param1: Int, param2: String, param3: String): DemoFragment4 =
             DemoFragment4().apply {
                 this.param1 = param1
                 this.param2 = param2
+                this.param3 = param3
             }
     }
 
@@ -24,7 +26,14 @@ class DemoFragment4: SupportFragment() {
         button.setOnClickListener {
             popTo(DemoFragment1::class.java, false)
         }
+
+        button2.text = param3
+
+        button2.setOnClickListener {
+            startWithPopTo(DemoFragment5.newInstance(5, "test"),
+                DemoFragment1::class.java, false)
+        }
     }
 
-    override fun getLayoutId(): Int = R.layout.fragment_demo
+    override fun getLayoutId(): Int = R.layout.fragment_demo4
 }
--- app/src/main/java/com/common/weikaiyun/demo/ui/DemoFragment5.kt
+++ app/src/main/java/com/common/weikaiyun/demo/ui/DemoFragment5.kt
@@ -7,12 +7,12 @@ import com.common.weikaiyun.fragmentation.SupportFragment
 import com.common.weikaiyun.fragmentation.fragmentargument.argument
 import kotlinx.android.synthetic.main.fragment_demo.*
 
-class DemoFragment3: SupportFragment() {
+class DemoFragment5: SupportFragment() {
     private var param1: Int by argument()
     private var param2: String by argument()
     companion object {
-        fun newInstance(param1: Int, param2: String): DemoFragment3 =
-            DemoFragment3().apply {
+        fun newInstance(param1: Int, param2: String): DemoFragment5 =
+            DemoFragment5().apply {
                 this.param1 = param1
                 this.param2 = param2
             }
@@ -21,9 +21,6 @@ class DemoFragment3: SupportFragment() {
     override fun initView(view: View, savedInstanceState: Bundle?) {
         title.text = "DemoFragment$param1"
         button.text = param2
-        button.setOnClickListener {
-            start(DemoFragment4.newInstance(4, "popTo1", "start5WithPopTo1"))
-        }
     }
 
     override fun getLayoutId(): Int = R.layout.fragment_demo
