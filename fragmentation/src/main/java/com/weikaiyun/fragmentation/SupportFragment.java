8aab7a76b1a37fb809306082c9896229c488c00d
weikaiyun <weikaiyun@pwrd.com>
2020-09-17 16:52:02 +0800

    框架优化

--- fragmentation/src/main/java/com/weikaiyun/fragmentation/SupportFragment.java
+++ fragmentation/src/main/java/com/weikaiyun/fragmentation/SupportFragment.java
@@ -4,9 +4,7 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
