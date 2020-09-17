bce91a79164ab07480aa9a0b0efbc9267ce68f4c
weikaiyun <weikaiyun@pwrd.com>
2020-09-17 20:14:51 +0800

    设置默认动画

--- app/src/main/java/com/common/weikaiyun/demo/DemoApplication.kt
+++ app/src/main/java/com/common/weikaiyun/demo/DemoApplication.kt
@@ -2,6 +2,7 @@ package com.common.weikaiyun.demo
 
 import android.app.Application
 import android.content.Context
+import com.common.weikaiyun.R
 import com.common.weikaiyun.demo.db.UserDatabase
 import com.weikaiyun.fragmentation.Fragmentation
 
@@ -15,6 +16,7 @@ class DemoApplication: Application() {
         Fragmentation.builder() // 设置 栈视图 模式为 （默认）悬浮球模式   SHAKE: 摇一摇唤出  NONE：隐藏， 仅在Debug环境生效
             .stackViewMode(Fragmentation.BUBBLE)
             .debug(true) // 实际场景建议.debug(BuildConfig.DEBUG)
+            .animation(R.anim.v_fragment_enter, R.anim.v_fragment_pop_exit, R.anim.v_fragment_pop_enter, R.anim.h_fragment_exit)
             .install()
     }
 
