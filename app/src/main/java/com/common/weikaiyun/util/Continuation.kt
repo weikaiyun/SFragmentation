89765fcd1826c6ac8c1b31bd44ca9fccde1cf22d
weikaiyun <wky19900505@gmail.com>
2020-10-03 19:14:29 +0800

    优化

--- app/src/main/java/com/common/weikaiyun/util/Continuation.kt
+++
@@ -1,7 +0,0 @@
-package com.common.weikaiyun.util
-
-abstract class Continuation<in T> : kotlin.coroutines.Continuation<T> {
-    abstract fun resume(value: T)
-    abstract fun resumeWithException(exception: Throwable)
-    override fun resumeWith(result: Result<T>) = result.fold(::resume, ::resumeWithException)
-}
