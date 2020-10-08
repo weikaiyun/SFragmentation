ceb9bcadd8772496d92944196b0d5d62787764e6
weikaiyun <wky19900505@gmail.com>
2020-10-08 11:06:58 +0800

    demo优化

--- app/src/main/java/com/common/weikaiyun/demo/db/UserDao.kt
+++ app/src/main/java/com/common/weikaiyun/demo/db/UserDao.kt
@@ -1,12 +1,11 @@
 package com.common.weikaiyun.demo.db
 
-import androidx.lifecycle.LiveData
 import androidx.room.*
 
 @Dao
 interface UserDao {
     @Query("SELECT * FROM user")
-    fun getAll(): LiveData<List<User>>
+    suspend fun getAll(): List<User>
 
     @Insert
     suspend fun insertAll(vararg users: User)
--- app/src/main/java/com/common/weikaiyun/demo/model/UserModel.kt
+++ app/src/main/java/com/common/weikaiyun/demo/model/UserModel.kt
@@ -4,8 +4,6 @@ import com.common.weikaiyun.demo.db.User
 import com.common.weikaiyun.demo.db.UserDatabase
 
 class UserModel {
-    suspend fun getAll() = UserDatabase.getInstance().userDao().getAll()
-
     suspend fun insertAll(vararg users: User) = UserDatabase.getInstance().userDao().insertAll(*users)
 
     suspend fun updateAll(vararg users: User) = UserDatabase.getInstance().userDao().updateAll(*users)
