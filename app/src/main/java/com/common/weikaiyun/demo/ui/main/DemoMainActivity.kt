d0a7001b0585a8df8ae8b0b9996183ceb2f8c07b
weikaiyun <wky19900505@gmail.com>
2020-10-11 16:32:26 +0800

    删除数据库 建议使用 https://github.com/MatrixDev/Roomigrant

--- app/src/main/java/com/common/weikaiyun/demo/ui/main/DemoMainActivity.kt
+++ app/src/main/java/com/common/weikaiyun/demo/ui/main/DemoMainActivity.kt
@@ -1,11 +1,8 @@
 package com.common.weikaiyun.demo.ui.main
 
 import android.os.Bundle
-import androidx.lifecycle.ViewModelProvider
 import com.common.weikaiyun.R
-import com.common.weikaiyun.demo.db.User
 import com.common.weikaiyun.demo.ui.base.BaseSupportActivity
-import com.common.weikaiyun.demo.viewmodel.UserViewModel
 import com.weikaiyun.fragmentation.SupportHelper
 
 class DemoMainActivity : BaseSupportActivity() {
@@ -13,15 +10,7 @@ class DemoMainActivity : BaseSupportActivity() {
     override fun getContentViewID(): Int = R.layout.activity_demo_main
 
     override fun initData(savedInstanceState: Bundle?) {
-        val viewModel = ViewModelProvider(this).get(UserViewModel::class.java)
 
-        val user1 = User(1, "1", 1, 1, 1, "1")
-        val user2 = User(2, "2", 2, 2, 2, "2")
-        val user3 = User(3, "3", 3, 3, 3, "3")
-        val user4 = User(4, "4", 4, 4, 4, "4")
-        val user5 = User(5, "5", 5, 5, 5, "5")
-
-        viewModel.updateAll(user1, user2, user3, user4, user5)
     }
 
     override fun initView(savedInstanceState: Bundle?) {
