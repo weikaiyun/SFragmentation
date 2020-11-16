bbc12598a2f7939b48228ef8a48977c057ee919c
weikaiyun <weikaiyun@pwrd.com>
2020-11-16 17:02:06 +0800

    优化

--- fragmentation_swipeback/src/main/java/com/weikaiyun/fragmentation/SwipeBackLayout.java
+++ fragmentation_swipeback/src/main/java/com/weikaiyun/fragmentation/SwipeBackLayout.java
@@ -8,7 +8,6 @@ import android.graphics.Rect;
 import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
 import android.util.DisplayMetrics;
-import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
